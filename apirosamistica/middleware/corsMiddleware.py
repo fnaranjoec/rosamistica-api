2bc8419b15d2ce15c1685b35a0d8495169792ea7
Fredy Naranjo <fnaranjoec@hotmail.com>
2019-12-24 11:16:44 -0500

    Add files via upload

---
+++ apirosamistica/middleware/corsMiddleware.py
@@ -0,0 +1,14 @@
+class CORSMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        response["Access-Control-Allow-Origin"] = "*"
+        return response
+
+
+#class corsMiddleware(object):
+#    def process_response(self, req, resp):
+#        resp["Access-Control-Allow-Origin"] = "*"
+#        return resp
